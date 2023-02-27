0debb22904069271179005705a6c8886f7fbe423
Seweryn Nurczyk <snurczyk99@gmail.com>
2023-02-27 16:48:42 +0100

    Add initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -59,10 +59,11 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
 
       console.log('new Product:', thisProduct);
     }
-    renderInMenu(){
+    renderInMenu() {
       const thisProduct = this;
       /* generate HTML based on template*/
       const generatedHTML = templates.menuProduct(thisProduct.data);
@@ -76,18 +77,46 @@
       /* add element to menu*/
       menuContainer.appendChild(thisProduct.element);
     }
+    initAccordion() {
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function (event) {
+
+        /* prevent default action for event */
+        event.preventDefault();
+
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(classNames.menuProduct.wrapperActive);
+        console.log(activeProduct);
+
+        /* W tym miejscu musze się przyznać do ściągania Panie Prezsie xd */  /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if ( ((activeProduct != null) && activeProduct.contains('active')) && (activeProduct != thisProduct.element) ) {
+          activeProduct.classlist.remove('active');
+        }
+
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle('active');
+
+      });
+
+
+    }
   }
   const app = {
     initMenu: function () {
       const thisApp = this;
       console.log('thisApp.data:', thisApp.data);
 
-      for (let productData in thisApp.data.products){
+      for (let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
       }
     },
 
-    initData: function (){
+    initData: function () {
       const thisApp = this;
 
       thisApp.data = dataSource;
